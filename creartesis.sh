a16e43200a970aa08977302a3e426677690b7ae6
Luis Octavio Ramírez Fernández <opengraphix@opengraphix.com.mx>
2017-03-16 20:17:39 -0600

    Ajuste en forma de cita en APA Español

--- creartesis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script para crear el archivo PDF de la tarea
-# Modifica las variable CAP Y TESIS de acuerdo a tus archivos
-CAP=capitulos/*.md
-TESIS=tesis.pdf
-#pandoc -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S --template plantilla_tesis.tex --filter pandoc-crossref -M "crossrefYaml=./pandoc-crossref.yaml" --filter pandoc-citeproc --csl estilos/apa.csl --bibliography capitulos/bibliografia.bib -o $TESIS $CAP
-pandoc -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S --template plantilla_tesis.tex --filter pandoc-citeproc --csl estilos/apa.csl --bibliography capitulos/bibliografia.bib -o $TESIS $CAP
